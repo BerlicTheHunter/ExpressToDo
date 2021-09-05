27a4126b6a31d663607784dd5fcd6374bc495a03
Berlic <bsorrell3@gmail.com>
2021-09-05 15:52:08 -0400

    added file structure

---
+++ server.js
@@ -0,0 +1,9 @@
+//...............Pull in required resources...............\\
+const express= require('express');
+const apiRoutes= require('./routes/apiRoutes');
+const htmlRoutes= require('./routes/htmlRoutes');
+
+
+
+const app= express();
+const PORT= process.env.PORT || 3001;
