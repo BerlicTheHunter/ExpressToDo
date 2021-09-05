154013f4c4e9a0c06c1c1a7c3a9931af41c3612d
Berlic <bsorrell3@gmail.com>
2021-09-05 17:25:26 -0400

    updated file structure. removed routes

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const express= require('express');
 const apiRoutes= require('./routes/apiRoutes');
 const htmlRoutes= require('./routes/htmlRoutes');
 
-
-
+// ..............Start Express...............\\
 const app= express();
+// ..............Set Port...............\\
 const PORT= process.env.PORT || 3001;
+
