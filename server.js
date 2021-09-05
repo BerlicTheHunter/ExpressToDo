c1c202e6ded16c5b1cb0921447ed09d839c90fea
Berlic <bsorrell3@gmail.com>
2021-09-05 17:34:57 -0400

    testing

--- server.js
+++ server.js
@@ -8,3 +8,7 @@ const app= express();
 // ..............Set Port...............\\
 const PORT= process.env.PORT || 3001;
 
+app.use(express.static('public'));
+app.listen(PORT,()=>
+  console.info(`Example app listening at http://localhost:${PORT}`)
+);
