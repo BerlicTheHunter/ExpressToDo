622f409631c41809da581180000af2e0ce3c77b9
Berlic <bsorrell3@gmail.com>
2021-09-07 21:39:57 -0400

    changed readfile path to dynamic

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,38 +1,36 @@
 // ...............Pull in required Modules...............\\
-const path= require("path");
-const router= require('express').Router();
-const fs= require("fs");
+const path = require("path");
+const router = require("express").Router();
+const fs = require("fs");
 
-
-router.get("/notes",(req, res)=>{
-  res.sendFile(path.join(__dirname, "../db/db.json"))
+router.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "../db/db.json"));
 });
 
+router.post("/notes", (req, res) => {
+  const dataBase = fs.readFileSync(path.join(__dirname, "../db/db.json"));
+  const { title, text } = req.body;
+  const id = Date.now();
 
-router.post("/notes",(req, res)=>{
-  const dataBase= fs.readFileSync("./db/db.json");
-  const { title, text} = req.body;
-  const id= Date.now()
   if (req.body) {
     const newNote = {
       title,
       text,
-      id: id.toString()
+      id: id.toString(),
     };
-    const parsedDB= JSON.parse(dataBase);
+
+    const parsedDB = JSON.parse(dataBase);
+
     parsedDB.push(newNote);
-    const stringDB= JSON.stringify(parsedDB);
-    
-    
-    fs.writeFile(path.join(__dirname, "../db/db.json"),stringDB,()=>{
-    res.json(`Note added successfully`);
+
+    const stringDB = JSON.stringify(parsedDB);
+
+    fs.writeFile(path.join(__dirname, "../db/db.json"), stringDB, () => {
+      res.json(`Note added successfully`);
     });
   } else {
-    res.error('Error in adding Note');
+    res.error("Error in adding Note");
   }
 });
-
-
-
 
 module.exports = router;
